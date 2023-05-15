e28a028a828ca21f07d15f5158a6bb39e037041e
nathanKith <ntolparov@gmail.com>
2023-05-15 23:28:33 +0300

    test project

---
+++ untitled/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from stocks import views as stock_views
+from django.urls import include, path, re_path
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+router = routers.DefaultRouter()
+router.register(r'stocks', stock_views.StockViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    path('admin/', admin.site.urls),
+]
